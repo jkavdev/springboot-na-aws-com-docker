661a69eb274d2664b45a62685622368974f1a713
jkavdev <99346554>
2019-02-11 23:38:32 -0200

    modulo 3 - Usando o ControllerAdvice para tratar argumentos inválidos

---
+++ beerstore/beerstore/src/main/java/br/com/jkavdev/hibicode/beerstore/error/ApiExceptionHandler.java
@@ -0,0 +1,55 @@
+package br.com.jkavdev.hibicode.beerstore.error;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class ApiExceptionHandler {
+
+    private static final String NO_MESSAGE_AVAILABLE = "No message available";
+    private static final Logger LOG = LoggerFactory.getLogger(ApiExceptionHandler.class);
+
+    private final MessageSource apiErrorMessageSource;
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception,
+                                                                               Locale locale) {
+
+        List<ErrorResponse.ApiError> apiErrors = exception.getBindingResult().getAllErrors().stream()
+                .map(ObjectError::getDefaultMessage)
+                .map(code -> toApiError(code, locale))
+                .collect(Collectors.toList());
+
+        ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST, apiErrors);
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    private ErrorResponse.ApiError toApiError(String code, Locale locale) {
+        String message;
+        try {
+            message = apiErrorMessageSource.getMessage(code, null, locale);
+        } catch (NoSuchMessageException e) {
+            //informa que nao pode achar uma messagem, e apresenta uma mesagem padrao
+            LOG.error("Could not find any message for {} code under {} locale", code, locale);
+            message = NO_MESSAGE_AVAILABLE;
+        }
+
+        return new ErrorResponse.ApiError(code, message);
+
+    }
+
+}
